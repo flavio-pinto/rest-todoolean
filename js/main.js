62b622547fab5d16b1a4515b23e29289a4e76142
Flavio Pinto <flaviopnt@hotmail.it>
2020-05-11 15:58:11 +0200

    Basic markup + js refs + init handlebars

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 /**api personale 157.230.17.132:3016/todos */
+
+$(document).ready(function () {
+    //Referenze
+    var todoInput = $('.todo__input');
+    var todoButton = $('.todo__button');
+    var todoList = $('.todo__list');
+
+    //Api
+    var apiUrl = '157.230.17.132:3016/todos';
+
+    //Init HandlebarsJs
+    var source = $('#template__results').html();
+    var template = Handlebars.compile(source);
+    
+}); //<---- end doc ready
+
+/********************
+ ******FUNZIONI******
+ ********************/
